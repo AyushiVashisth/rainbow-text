e6b27aa0f3d9ce21fac721a8c9c5d5102aaac751
Ayushi Vashisth <ayushivashisth22@gmail.com>
2024-07-25 20:05:51 +0530

    fix the error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+export class Log {
+  static success(msg) {
+    console.log(`%c ${msg}`, "color: green");
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  static danger(msg) {
+    console.log(`%c ${msg}`, "color: red");
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  static info(msg) {
+    console.log(`%c ${msg}`, "color: black; background: yellow");
+  }
+}
